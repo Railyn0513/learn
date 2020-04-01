b3f275517df3d72fc8c28bd9ebc306b078f5067b
Railyn Wu <railynwu@outlook.com>
2020-04-01 11:46:02 +0100

    changed something

---
+++ Titanic_Survival.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+from os import rename
+
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("Word"))
+print(greet("Railyn"))
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
